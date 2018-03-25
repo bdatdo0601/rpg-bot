58805e24554d89b765cded997b3acd0a4fcefee4
Chris Ligonde <ligondec@.wit.edu>
2018-03-25 17:19:59 -0400

    initial commit

---
+++ RPG Discord Bot.py
@@ -0,0 +1,53 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import json
+import io
+import os
+
+game_status = 0
+Client = discord.Client()
+client = commands.Bot(command_prefix = "")
+
+async def handleStart(message):
+    if (game_status == 0):
+        await client.send_message(message.channel, "Participant players type 'entering'")
+        global game_status
+        game_status = 1
+
+    elif (game_status == 1):
+        await client.send_message(message.channel, "Game initialization already in progress")
+
+async def addPlayers(message):
+    if(game_status==0):
+        await client.send_message(message.channel, "No active game")
+    elif(game_status==1):
+        print(message.author)
+
+async def handleLeaveGame(message):
+    if (game_status == 0):
+        await client.send_message(message.channel, "No active game")
+    else:
+        await client.send_message(message.channel, "Player has left the game lobby")
+
+@client.event
+async def on_ready():
+    print("Bot is ready!")
+
+@client.event
+async def on_message(message):
+    print(message.content)
+    if message.content == "Start game":
+        await handleStart(message)
+
+    elif message.content == "entering":
+        await addPlayers(message)
+
+    elif message.content == "Exit lobby":
+       await handleLeaveGame(message)
+
+
+
+client.run("NDI3MTY0NjM4MDA2NzM4OTY1.DZg8uQ.v9tarVkSpDBMpJ6uJmL3MrfKXPE")
